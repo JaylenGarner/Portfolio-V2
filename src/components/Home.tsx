8ccaa635e8d70737934f445366351171fd227f53
Jaylen Garner <jaylen@moonraydevelopment.com>
2023-12-14 18:04:00 -0500

    Background changes, began configuring nav items

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,24 +7,31 @@ const Home = () => {
   return (
     <>
     {/* Reveal */}
-    <motion.div className="absolute z-50 w-screen h-screen flex justify-center items-center bg-black"
+    <motion.div className="absolute z-50 w-screen h-[100dvh] flex justify-center items-center bg-black"
     initial={{opacity: 1}}
     animate={{opacity: 0}}
-    transition={{duration: 3, delay: .5}}
+    transition={{duration: 3, delay: 1}}
     >
     <h1 className="text-xl">Jaylen Garner</h1>
     </motion.div>
 
     {/* Tint */}
-    <div className="absolute z-30 w-screen h-screen bg-black bg-blend-darken opacity-30">
+    <div className="absolute z-30 w-screen h-screen bg-black bg-blend-darken opacity-50">
 
     </div>
 
     {/* Content */}
-    <div className="absolute z-40 w-screen h-screen flex justify-center items-center">
-        <h1 className="text-xl">
-        Jaylen Garner
-        </h1>
+    <div className="absolute z-40 w-screen h-[100dvh] flex justify-center items-center">
+        <h1 className="text-xl">Jaylen Garner</h1>
+
+        <h2 className="absolute top-4 left-4 text-lg">Software Engineer</h2>
+
+        <h2 className="absolute top-4 right-4 text-lg">Business</h2>
+
+        <h2 className="absolute bottom-4 left-4 text-lg">Socials</h2>
+
+        <h2 className="absolute bottom-4 right-4">Contact</h2>
+
 
     </div>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,43 @@ export default function Page(props) {
 // Canvas components go here
 // It will receive same props as the Page component (from getStaticProps, etc.)
 Page.canvas = (props) =>
+
 <ShaderGradient
+uSpeed={.3}
+uStrength={1.7}
+color2='89CFF0'
+color1='000000'
+color3='474747'
+/>
+
+{/* <ShaderGradient
+type={"plane"}
+animate={"on"}
+uTime={0}
+uSpeed={0.3}
+uStrength={1.5}
+uDensity={0.8}
+uFrequency={5.5}
+uAmplitude={1}
 positionX={-0.1}
-rotationY={130}
-rotationZ={70}
-cAzimuthAngle={270}
-cPolarAngle={180}
-cDistance={5} />
+positionY={0}
+positionZ={0}
+rotationX={0}
+rotationY={0}
+rotationZ={45}
+color1={"000000"}
+color2={"FF0000"}
+color3={"0000FF"}
+reflection={0.6}
+shader={"defaults"}
+dampingFactor={0}
+cAzimuthAngle={0}
+cPolarAngle={0}
+cDistance={0.5}
+cameraZoom={0}
+// brightness={0.8}
+// envPreset={"dawn"}
+/> */}
 
 
 export async function getStaticProps() {
